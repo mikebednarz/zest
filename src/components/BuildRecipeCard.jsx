015aabea90b8a68ba82560d86dea91daeb2bcc6e
mikebednarz <mikebednarz001@gmail.com>
2023-08-25 21:40:33 -0400

    adding groundwork for recipe sorting functionality

--- src/components/BuildRecipeCard.jsx
+++ src/components/BuildRecipeCard.jsx
@@ -4,12 +4,31 @@ import CreateRecipePopup from "./CreateRecipePopup";
 const BuildRecipeCard = () => {
   const [cardPopup, setCardPopup] = useState(false);
 
+  const handleOptionChange = () => {
+    const dropdownBox = document.getElementById('dropdown');
+    const selectedOption = dropdownBox?.value;
+
+    if (selectedOption === 'order-added') {
+      // dispatch(updateRenderOrderByAdded())
+    } else if (selectedOption === 'titleA') {
+      // dispatch(updateRenderOrderByTitleA())
+    } else {
+      // dispatch(updateRenderOrderByTitleD())
+    }
+  }
+
   return (
     <div className="container">
-      <div>
-        <h3 className="createRecipe">Build Recipe Card:</h3>
+      <div className="dropdowns-container">
+          <span className="sort-by">Sort By</span>
+          <select id='dropdown' onChange={handleOptionChange} className="dropdown">
+            <option value='order-added'>Order Added</option>
+            <option value='titleA'>Title - Ascending</option>
+            <option value='titleD'>Title - Descending</option>
+          </select>
       </div>
-    
+        <h3 className="createRecipe">Build Recipe Card:</h3>
+
       <div className="webflow-style-input">
         <input className="recipeName" type="text" placeholder="Recipe Name"></input>
         <button type="submit"><i className="icon ion-android-arrow-forward"></i></button>
